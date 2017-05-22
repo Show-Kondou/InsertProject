7625c05e27c4056345aa7a8749dee527a068cfe4
N.R <4792doe0@gmail.com>
2017-05-22 16:09:02 +0900

    背景追加_野嶋

---
+++ Program/Project/InsertProject/Assets/Nojima/Scripts/BGScroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour {
+
+    [SerializeField]
+    float ScrollSpeed = 10f;
+    MeshRenderer meshRender;
+
+    float Offset = 0f;
+
+	// Use this for initialization
+	void Start () {
+		meshRender = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Offset = Mathf.Repeat(Time.time * ScrollSpeed, 1f);
+
+        meshRender.material.SetTextureOffset("_MainTex", new Vector2(0f, Offset));
+	}
+}
