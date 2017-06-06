7b3e45edd9d0632d053f2af6aee31ec83cedf694
SasakiMizuki <mizuki.ssk.halnagoya@gmail.com>
2017-06-07 02:09:43 +0900

    スライム系更新(未完成)‗ssk

---
+++ Program/Project/InsertProject/Assets/Users_ここに自分のフォルダ移動して/Mizuki/Script/SandwichObj/CEnemyMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CEnemyMove : CSSandwichObject {
+	// Use this for initialization
+	void Start() {
+		m_OrderNumber = 0;
+		ObjectManager.Instance.RegistrationList(this, m_OrderNumber);
+
+	}
+
+	public override void Execute(float deltaTime) {
+		base.Execute(deltaTime);
+
+	}
+
+	public override void LateExecute(float deltaTime) {
+		base.LateExecute(deltaTime);
+
+	}
+}
