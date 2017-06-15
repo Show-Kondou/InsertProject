4ed05e686c10f45dd030abb2cf1e1c101fc26bcb
RYUKI\nhs40134 <4792doe0@gmail.com>
2017-06-15 20:28:45 +0900

    リザルト追加＿野嶋

---
+++ Program/Project/InsertProject/Assets/Users_ここに自分のフォルダ移動して/Nojima/Scripts/Result/ButtonsClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsClick : MonoBehaviour {
+
+    public void TitleButton()
+    {
+        print("タイトル");
+    }
+
+    public void RetryButton() {
+        print("リトライ");
+    }
+}
