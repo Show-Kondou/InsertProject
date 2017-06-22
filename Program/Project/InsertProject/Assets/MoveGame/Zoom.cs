939c2b485f943eede57e54c5e874eebd9e7b705b
Show Koudou <mail.show.Kondou@gmail.com>
2017-06-22 10:08:39 +0900

    ズーム追加_Show

---
+++ Program/Project/InsertProject/Assets/MoveGame/Zoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour {
+
+	public Transform m_Target;
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetKey( KeyCode.Z ) ) {
+			ZoomMove();
+		}
+	}
+
+
+	private void ZoomMove() {
+		var pos = transform.position;
+		var sub = (m_Target.position - pos) / 8.0F * Time.deltaTime * 10.0F;
+		pos += sub;
+		transform.position = pos;
+	}
+}
