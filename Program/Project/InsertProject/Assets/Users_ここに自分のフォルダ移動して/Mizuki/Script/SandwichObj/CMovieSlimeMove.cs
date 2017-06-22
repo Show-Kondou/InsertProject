ef00c70825eabca5de887372a1d6d7720a0e8fc6
SasakiMizuki <mizuki.ssk.halnagoya@gmail.com>
2017-06-22 17:38:06 +0900

    動画用変更、デスクトップにデータ移行_ssk

--- Program/Project/InsertProject/Assets/Users_ここに自分のフォルダ移動して/Mizuki/Script/SandwichObj/CMovieSlimeMove.cs
+++ Program/Project/InsertProject/Assets/Users_ここに自分のフォルダ移動して/Mizuki/Script/SandwichObj/CMovieSlimeMove.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CSlimeMove : CSSandwichObject {
+public class CMovieSlimeMove : CSSandwichObject {
 	public enum SLIME_TYPE {
 		Ally,
 		Enemy,
@@ -15,21 +15,21 @@ public class CSlimeMove : CSSandwichObject {
 
 	public SLIME_TYPE myType;
 	[SerializeField]
-	private GameObject SlimeMesh;	// スライムのモデル
+	private GameObject SlimeMesh;   // スライムのモデル
 	[SerializeField]
-	private Material EnemyMat;		// 敵マテリアル
+	private Material EnemyMat;      // 敵マテリアル
 	[SerializeField]
-	private Material AllyMat;		// 味方マテリアル
+	private Material AllyMat;       // 味方マテリアル
 	[SerializeField]
-	private Material FeverMat;		// フィーバーマテリアル
+	private Material FeverMat;      // フィーバーマテリアル
 	[SerializeField]
-	private GameObject FlagObj;		// 旗モデル
+	private GameObject FlagObj;     // 旗モデル
 	static public int EnemyNum = 0; // 敵の総数
 	[SerializeField]
 	private float NothingLifeTime = 1.0f;   // 設定時間
 	[SerializeField]
-	private CFeverSlimeMovePointManager pointMng;	// フィーバースライム目的地
-	private float NothingLifeTimer;	// 3段階目になった時に時間で死亡させる。
+	private CFeverSlimeMovePointManager pointMng;   // フィーバースライム目的地
+	private float NothingLifeTimer; // 3段階目になった時に時間で死亡させる。
 
 	// Use this for initialization
 	void Start() {
@@ -37,12 +37,12 @@ public class CSlimeMove : CSSandwichObject {
 		m_OrderNumber = 0;
 		ObjectManager.Instance.RegistrationList(this, m_OrderNumber);
 		m_Moving = false;
-		m_Position = transform.position;							// 初期位置に移動
+		m_Position = transform.position;                            // 初期位置に移動
 		m_MoveTimer = m_WaitTime;                                   // 移動待ち時間
-		//m_Rotation = Random.Range(0, 360 * Mathf.PI / 180);         // 初期向きをランダムで決定
+																	//m_Rotation = Random.Range(0, 360 * Mathf.PI / 180);         // 初期向きをランダムで決定
 		m_Rotation = 90.0f;         // 初期向きをランダムで決定
 		Debug.Log(m_Rotation);
-		transform.rotation = Quaternion.Euler(0, 0, m_Rotation);	// 向きを設定
+		transform.rotation = Quaternion.Euler(0, 0, m_Rotation);    // 向きを設定
 		FlagObj.SetActive(false);
 		// 生成されたスライムごとにマテリアルと自身のタイプを設定、初期化
 		if(transform.tag == "Enemy") {
@@ -52,14 +52,14 @@ public class CSlimeMove : CSSandwichObject {
 		} else if(transform.tag == "Ally") {
 			myType = SLIME_TYPE.Ally;
 			SlimeMesh.GetComponent<Renderer>().material = AllyMat;
-		} else if(transform.tag == "Fever"){
+		} else if(transform.tag == "Fever") {
 			myType = SLIME_TYPE.Fever;
 			SlimeMesh.GetComponent<Renderer>().material = FeverMat;
 			FlagObj.SetActive(true);
 			pointMng = GameObject.Find("FeverSlimeMovePoints").GetComponent<CFeverSlimeMovePointManager>();
-			pointMng.ChangeNextPosition(0);	// 初期目的地設定
+			pointMng.ChangeNextPosition(0); // 初期目的地設定
 			transform.position = pointMng.FeverSlimeDestination;    // 初期目的地移動
-		}else {
+		} else {
 			myType = SLIME_TYPE.Big;
 			SlimeMesh.GetComponent<Renderer>().material = AllyMat;
 		}
@@ -72,7 +72,7 @@ public class CSlimeMove : CSSandwichObject {
 		//=============
 		if(!this)
 			return; // バグ回避用。要修正
-		//=============
+					//=============
 
 		if(myType == SLIME_TYPE.Nothing) {
 			NothingLifeTimer -= deltaTime;
@@ -99,12 +99,12 @@ public class CSlimeMove : CSSandwichObject {
 		m_MoveTimer -= deltaTime;
 		// 待ち時間が0になったらジャンプ
 		if(m_MoveTimer < 0) {
-			CSoundManager.Instance.PlaySE( AUDIO_LIST.SE_SLIME_CONVERT );
+			CSoundManager.Instance.PlaySE(AUDIO_LIST.SE_SLIME_CONVERT);
 			m_Moving = true;                    // 移動中フラグオン
 			m_MoveTimer = m_WaitTime;           // 待ち時間再入
 			m_Position = transform.position;    // 位置更新
 			m_JumpTimer = 0;                    // ジャンプ経過時間タイマーをリセット
-			// 自身のタイプごとに動作を変更
+												// 自身のタイプごとに動作を変更
 			switch(myType) {
 				case SLIME_TYPE.Big:
 				case SLIME_TYPE.Ally:
@@ -129,7 +129,7 @@ public class CSlimeMove : CSSandwichObject {
 				default:
 					break;
 			}
-			transform.rotation = Quaternion.Euler(0, 0, m_Rotation * Mathf.Rad2Deg - 180);	// 向きを変える
+			transform.rotation = Quaternion.Euler(0, 0, m_Rotation * Mathf.Rad2Deg - 180);  // 向きを変える
 		}
 	}
 
@@ -142,8 +142,13 @@ public class CSlimeMove : CSSandwichObject {
 	/// </summary>
 	public override void SandwichedAction() {
 		if(myType == SLIME_TYPE.Enemy) {
-			SameTimeSandObjNum();
-			ChangeSlimeState(SLIME_TYPE.Ally);
+			myType = SLIME_TYPE.Nothing;   // 属性を味方に
+			m_Invincible = true;            // 無敵オン
+			m_InvincibleTimer = 1.0f;       // 無敵時間
+			SlimeMesh.SetActive(false);
+			// パーティクルを出す
+			CSParticleManager.Instance.Play(CSParticleManager.PARTICLE_TYPE.AllySlimeDeath,
+				transform.position);
 		} else if(myType == SLIME_TYPE.Ally) {
 			SameTimeSandObjNum();
 			myType = SLIME_TYPE.Nothing;   // 属性を味方に
@@ -151,7 +156,8 @@ public class CSlimeMove : CSSandwichObject {
 			m_InvincibleTimer = 1.0f;       // 無敵時間
 			SlimeMesh.SetActive(false);
 			// パーティクルを出す
-			CSParticleManager.Instance.Play(CSParticleManager.PARTICLE_TYPE.AllySlimeDeath, transform.position);
+			CSParticleManager.Instance.Play(CSParticleManager.PARTICLE_TYPE.AllySlimeDeath, 
+				transform.position);
 		} else if(myType == SLIME_TYPE.Nothing) {
 			SameTimeSandObjNum();
 		} else if(myType == SLIME_TYPE.Big) {
@@ -167,7 +173,7 @@ public class CSlimeMove : CSSandwichObject {
 				m_Invincible = true;            // 無敵オン
 				m_InvincibleTimer = 1.0f;       // 無敵時間
 				SlimeMesh.GetComponent<Renderer>().material = EnemyMat;  // マテリアル変更
-				myType = SLIME_TYPE.Enemy;		// 属性を敵に
+				myType = SLIME_TYPE.Enemy;      // 属性を敵に
 				m_PressObjList.Clear();
 				EnemyNum++;
 				break;
