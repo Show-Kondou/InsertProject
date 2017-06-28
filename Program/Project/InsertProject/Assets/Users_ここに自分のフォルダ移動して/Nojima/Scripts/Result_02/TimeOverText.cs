f52eddf8874f1371724aa2d3c5877ccd1482a69a
RYUKI\nhs40134 <4792doe0@gmail.com>
2017-06-29 04:27:17 +0900

    タイムオーバー追加＿野嶋

---
+++ Program/Project/InsertProject/Assets/Users_ここに自分のフォルダ移動して/Nojima/Scripts/Result_02/TimeOverText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeOverText : MonoBehaviour {
+
+    float MySize = 1f;
+    bool bScaler = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(MySize <= 1f)
+            bScaler = true;
+        if(MySize >= 1.1f)
+            bScaler = false;
+
+        if (bScaler)
+        {
+            MySize += 0.1f * Time.deltaTime;
+        }
+        else
+            MySize -= 0.1f * Time.deltaTime;
+
+        transform.localScale = new Vector3(MySize, MySize);
+	}
+}
