10a0aca8b97e31b24caeb42b624d366d2e54dc66
nagashima_tatsuya <tatsuya1821@gmail.com>
2017-06-29 19:44:36 +0900

    ボスUIのアルファ値ゼロにするスクリプト追加＿野嶋

---
+++ Program/Project/InsertProject/Assets/Users_ここに自分のフォルダ移動して/Nojima/Scripts/Boss/BGAlphaZero.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BGAlphaZero : MonoBehaviour
+{
+
+    [SerializeField]
+    Image[] BossTexture;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < BossTexture.Length; i++)
+            BossTexture[i].material.color =
+                new Color(BossTexture[i].material.color.r, BossTexture[i].material.color.g, BossTexture[i].material.color.b, 0f);
+    }
+
+}
