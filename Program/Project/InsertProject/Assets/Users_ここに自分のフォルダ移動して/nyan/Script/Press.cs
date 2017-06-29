699f651268af7b6c2ee259e7cc9a65e546c89849
Genki Wakayama <Hartmann4504@icloud.com>
2017-06-29 10:14:53 +0900

    プレス機バグ修正_nyan

--- Program/Project/InsertProject/Assets/Users_ここに自分のフォルダ移動して/nyan/Script/Press.cs
+++ Program/Project/InsertProject/Assets/Users_ここに自分のフォルダ移動して/nyan/Script/Press.cs
@@ -24,6 +24,7 @@ public class Press : MonoBehaviour {
 
     //移動関連
     private Vector3 vSpeed = new Vector3(3.0f, 3.0f, 3.0f);
+    //private Vector3 vSpeed = new Vector3(0.1f, 0.1f, 0.1f);
     private float fRad;                 //進行方向計算用
     private Vector3 vMovePos;           //移動用
     private float fGrace = 0.025f;      //差
