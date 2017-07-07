8ea08d9e239fceffe816e541bf37b16c8808897c
SasakiMizuki <mizuki.ssk.halnagoya@gmail.com>
2017-07-07 10:37:01 +0900

    一時コミット_ssk

---
+++ Program/Project/InsertProject/Assets/Game/Script/Camera/ChangeBrightness.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBrightness : ObjectBase {
+	private int BrightnessDir;	// 明るくするか暗くするか(1or-1)
+
+	// Use this for initialization
+	void Start() {
+		m_OrderNumber = 0;
+		ObjectManager.Instance.RegistrationList(this, m_OrderNumber);
+		BrightnessDir = -1;
+
+		this.enabled = false;
+	}
+
+	public override void Execute(float deltaTime) {
+
+	}
+
+	public override void LateExecute(float deltaTime) {
+
+	}
+
+
+}
