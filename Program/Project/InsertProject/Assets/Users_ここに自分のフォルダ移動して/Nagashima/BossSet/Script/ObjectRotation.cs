52ec1eb83fffea430131fa49aeaed117bc273bb1
nagashima_tatsuya <tatsuya1821@gmail.com>
2017-07-07 09:57:29 +0900

    ワームホール変更と、ボスの移動方法変更_nagashima

---
+++ Program/Project/InsertProject/Assets/Users_ここに自分のフォルダ移動して/Nagashima/BossSet/Script/ObjectRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour 
+{
+    [SerializeField]
+    private Vector3 Rotation;
+
+	// ===== スタート =====
+	void Start () 
+    {
+		
+	}
+	
+	// ===== 更新 =====
+	void Update () 
+    {
+        // オブジェクトを回転させる
+        this.transform.Rotate(Rotation.x, Rotation.y, Rotation.z);
+	}
+}
